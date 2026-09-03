1d63d0293f2bd9d7402815947f15cf57cb4a0c10
marcobelligoli <marco.belligoli98@gmail.com>
2026-09-03 19:18:04 +0200

    First SpringBoot project commit

---
+++ src/test/java/io/github/marcobelligoli/sentra/SentraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marcobelligoli.sentra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
